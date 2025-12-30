8141866a45cfd3dc5f5496d72dd4822b1a4ab0e0
Anmol Prasad <anmol.gpc@gmail.com>
2025-12-30 18:07:25 +0530

    Added Swagger JWT authorization support

---
+++ src/main/java/com/anmol/vroom/security/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.anmol.vroom.security.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "VROOM API",
+                version = "1.0",
+                description = "Ride-sharing backend APIs"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
